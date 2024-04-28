704d9c30b762cee6332ed06791aadfce7d0c90e0
kackey0-1 <kentaro.a.kakimoto@gmail.com>
2024-04-28 14:23:58 +0900

    first commit

---
+++ README.md
@@ -0,0 +1,23 @@
+# E-hon Recording App
+
+## 開発環境
+
+- Java 21
+- Kotlin & Spring Boot 2.5.5
+- SonarCloud
+- Typescript & React
+
+## Description
+
+絵本を読んだ記録を残すアプリ
+
+## 主な機能
+
+- Socialログイン(Google)
+- 絵本の読書記録
+- 読書記録の一覧表示
+- 集計画面
+
+
+
+
---
+++ src/main/kotlin/com/hypo/driven/recording/RecordingApplication.kt
@@ -0,0 +1,11 @@
+package com.hypo.driven.recording
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class RecordingApplication
+
+fun main(args: Array<String>) {
+    runApplication<RecordingApplication>(*args)
+}
